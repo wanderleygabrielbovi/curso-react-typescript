b6c3f98dcfa060473295275a3de94aee98a5e63a
Wanderley Gabriel Bovi <wanderleygabrielbovi@gmail.com>
2025-02-06 22:47:52 -0300

    add: coins api

--- criptoapp/src/pages/home/index.tsx
+++ criptoapp/src/pages/home/index.tsx
@@ -1,14 +1,70 @@
+import { useState, FormEvent, useEffect } from 'react';
 import styles from './home.module.css';
 import { BsSearch } from 'react-icons/bs';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { CoinProps } from '../../utils/interfaces';
+import { DataProp } from '../../utils/interfaces';
 
 export function Home() {
+    const [input, setInput] = useState("");
+    const [coins, setCoins] = useState<CoinProps[]>([]);
+
+    useEffect(() => {
+      getData();
+    }, [])
+
+    async function getData() {
+      fetch("https://api.coincap.io/v2/assets?limit=10&offset=0")
+      .then(response => response.json())
+      .then((data: DataProp) => {
+        const coinsData = data.data;
+        
+        const price = Intl.NumberFormat("en-US", {
+          style: "currency",
+          currency: "USD"
+        })
+
+        const priceCompact = Intl.NumberFormat("en-US", {
+          style: "currency",
+          currency: "USD",
+          notation: "compact"
+        })
+
+        const formatedResult = coinsData.map((item) => {
+          const formated = {
+            ...item,
+            formatedPrice: price.format(Number(item.priceUsd)),
+            formatedMarket: priceCompact.format(Number(item.priceUsd)),
+            formatedVolume: priceCompact.format(Number(item.volumeUsd24Hr))
+          }
+
+          return formated;
+        })
+
+        setCoins(formatedResult);
+      })
+    }
+
+    const navigate = useNavigate();
+
+    function handleSubmit(e: FormEvent) {
+      e.preventDefault();
+
+      navigate(`/datail/${input}`)
+    }
+
+    function handleGetMore() {
+
+    }
+
     return (
       <main className={styles.container}>
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={handleSubmit}>
           <input 
             type="text"
             placeholder="Digite o nome da moeda..."
+            value={input}
+            onChange={ (e) => setInput(e.target.value) }
             />
             <button type="submit">
               <BsSearch size={30} color="#FFF" />
@@ -27,34 +83,49 @@ export function Home() {
           </thead>
 
           <tbody id="tbody">
-            <tr className={styles.tr}>
+            
+          {coins.length > 0 && coins.map((item) => (
+            <tr className={styles.tr} key={item.id}>
+              
               <td className={styles.tdLabel} data-label="Moeda">
                 <div className={styles.name}>
-                  <Link to={"/detail/bitcoin"}>
-                  <span>Bitcoin</span> | BTC
+                  <img 
+                  className={styles.logo}
+                    src={`https://assets.coincap.io/assets/icons/${item.symbol.toLowerCase()}@2x.png`} 
+                    alt="Logo Cripto" 
+                  />
+                  <Link to={`/detail/${item.id}`}>
+                  <span>{item.name}</span> | {item.symbol}
                 </Link>
                 </div>
               </td>
 
               <td className={styles.tdLabel} data-label="Valor mercado">
-                1T
+                {item.formatedMarket}
               </td>
 
               <td className={styles.tdLabel} data-label="Preco">
-                8.000
+                {item.formatedPrice}
               </td>
 
               <td className={styles.tdLabel} data-label="Volume">
-                2B
+                {item.formatedVolume}
               </td>
 
-              <td className={styles.tdLabel} data-label="Mudanca 24h">
-                <span>1.20</span>
+              <td className={Number(item.changePercent24Hr) > 0 ? styles.tdProfit : styles.tdLoss} data-label="Mudanca 24h">
+                <span>{Number(item.changePercent24Hr).toFixed(3)}</span>
               </td>
-              
+            
             </tr>
+          ))}
+
           </tbody>
         </table>
+
+        <button className={styles.buttonMore} onClick={handleGetMore}>
+          Carregar mais
+        </button>
+
       </main>
     )
   }
---
+++ criptoapp/src/utils/interfaces.tsx
@@ -0,0 +1,21 @@
+export interface CoinProps{
+    id: string;
+    name: string;
+    symbol: string;
+    priceUsd: string;
+    vwap24Hr: string;
+    changePercent24Hr: string;
+    rank: string;
+    supply: string;
+    maxSupply: string;
+    marketCapUsd: string;
+    volumeUsd24Hr: string;
+    explorer: string;
+    formatedPrice?: string;
+    formatedMarket?: string;
+    formatedVolume?: string;
+}
+
+export interface DataProp{
+    data: CoinProps[]
+}
